940dc21df3d275f6d70c629419fd846c9e21571f
Sharif Ezzat <goodsharif@gmail.com>
2022-03-18 18:50:07 -0700

    init

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,29 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+
+// $(function(){  // $(document).ready shorthand
+//   $('.fade-1').delay(10).animate({'opacity':'1'},700);
+//   $('.fade-2').delay(500).animate({'opacity':'1'},700);
+//   $('.fade-3').delay(1000).animate({'opacity':'1'},700);
+// });
+
+
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * .5);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1', 'padding-top': '-10px'},350);
+            }
+        });
+    });
+});
--- webpack.common.js
+++ webpack.common.js
@@ -3,55 +3,74 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const AssetsPlugin = require("assets-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     main: path.join(__dirname, "src", "index.js"),
-    cms: path.join(__dirname, "src", "js", "cms.js"),
   },
 
   output: {
-    path: path.join(__dirname, "dist")
+    path: path.join(__dirname, "dist"),
+    publicPath: "",
   },
 
   module: {
     rules: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        loader: "file-loader",
+        options: {
+          name: "name=/[hash].[ext]",
+        },
       },
+
+      {test: /\.json$/, loader: "json-loader"},
+
       {
         loader: "babel-loader",
         test: /\.js?$/,
         exclude: /node_modules/,
-        query: {cacheDirectory: true}
+        options: {cacheDirectory: true},
       },
+
       {
         test: /\.(sa|sc|c)ss$/,
         exclude: /node_modules/,
-        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
-      }
-    ]
+        use: [
+          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false
+            }
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
   },
 
   plugins: [
+    new webpack.ProvidePlugin({
+      fetch:
+        "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch",
+    }),
+
     new AssetsPlugin({
       filename: "webpack.json",
       path: path.join(process.cwd(), "site/data"),
-      prettyPrint: true
+      prettyPrint: true,
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts/",
-        to: "fonts/",
-        flatten: true
-      }
-    ]),
-    new HtmlWebpackPlugin({
-      filename: 'admin/index.html',
-      template: 'src/cms.html',
-      inject: false,
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/fonts/",
+          to: "fonts/",
+        },
+      ],
     }),
-  ]
+  ],
 };
